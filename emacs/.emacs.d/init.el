50d789f1109e7cb0dcc4d193abeb01b0229e78dd
Katherine Surta <kate.surta@gmail.com>
2018-05-24 15:29:37 +0300

    update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,11 +24,13 @@
       neotree
       py-autopep8
       py-import-check
+      smart-mode-line
+      smart-mode-line-powerline-theme
       terraform-mode
       yaml-mode ))
   (unless (package-installed-p pkg)
     (package-install pkg)))
-	   
+
 (require 'evil)
 (evilnc-default-hotkeys)
 
@@ -56,7 +58,9 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (deeper-blue)))
- '(custom-safe-themes (quote (default)))
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(evil-mode t)
  '(global-evil-matchit-mode t)
  '(global-evil-surround-mode t)
@@ -65,15 +69,28 @@
  '(initial-scratch-message ";; Happy coding! ^_^")
  '(package-selected-packages
    (quote
-    (dockerfile-mode terraform-mode yaml-mode ipython flycheck elpy py-import-check neotree multiple-cursors evil-surround evil-nerd-commenter evil-matchit evil-args)))
+    (smart-mode-line smart-mode-line-powerline-theme dockerfile-mode terraform-mode yaml-mode ipython flycheck elpy py-import-check neotree multiple-cursors evil-surround evil-nerd-commenter evil-matchit evil-args)))
+ '(show-trailing-whitespace t)
+ '(sml/theme (quote powerline))
  '(tool-bar-mode nil))
+
+(sml/setup)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(sml/col-number ((t (:inherit sml/global :background "dim gray"))))
+ '(sml/filename ((t (:inherit sml/global :background "grey17" :foreground "DarkSlateGray1" :weight bold))))
+ '(sml/folder ((t (:inherit sml/global :background "grey17" :foreground "DarkSlateGray4" :weight normal))))
+ '(sml/line-number ((t (:inherit sml/global :background "black" :foreground "lemon chiffon" :weight bold))))
+ '(sml/modes ((t (:inherit sml/global :background "gray40" :foreground "lemon chiffon"))))
+ '(sml/modified ((t (:inherit sml/not-modified :foreground "indian red" :weight bold))))
+ '(sml/position-percentage ((t (:inherit sml/prefix :background "gray40" :foreground "lemon chiffon" :weight normal))))
+ '(sml/prefix ((t (:inherit sml/global :background "grey17" :foreground "PaleTurquoise4"))))
+ '(sml/vc ((t (:inherit sml/git :background "grey40" :foreground "CadetBlue1"))))
+ '(sml/vc-edited ((t (:inherit sml/prefix :background "gray40" :foreground "RosyBrown1")))))
 
 (set-face-attribute 'default nil :font "Inconsolata-15")
 (set-frame-font "Inconsolata-15" nil t)
